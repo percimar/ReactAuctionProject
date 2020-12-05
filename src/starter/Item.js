c63c641f595834c0195c9c493a70cac9ff9ce534
Asmar Siddiqui <60093530@cna-qatar.edu.qa>
2020-12-05 03:38:06 +0300

    displays comments and replies, users can leave comments

--- src/starter/Item.js
+++ src/starter/Item.js
@@ -12,19 +12,23 @@ import { makeStyles } from "@material-ui/core/styles";
 import styles from "../assets/jss/material-kit-react/views/loginPage.js";
 import UserContext from '../UserContext'
 import ItemForm from './ItemForm'
-
+import Collapse from '@material-ui/core/Collapse';
+import TextField from '@material-ui/core/TextField';
+import CardContent from '@material-ui/core/CardContent';
+import SendIcon from '@material-ui/icons/Send';
 import IconButton from "@material-ui/core/IconButton";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Close from "@material-ui/icons/Close";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Slide from "@material-ui/core/Slide";
 import CustomInput from "../components/CustomInput/CustomInput.js";
 import { useHistory } from 'react-router-dom';
 
 import db from '../db'
-
+import Comment from '../Asmar/Comment'
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="down" ref={ref} {...props} />;
@@ -45,6 +49,32 @@ export default function Item({ auctionId, id, name, description, picture }) {
 
     const history = useHistory()
 
+    const [comments, setComments] = useState([]);
+    useEffect(() => db.Auctions.Items.Comments.listenToOneItemAllComments(setComments, auctionId, id), [])
+
+    // useEffect(() => comments.map(comment => {
+    //     const getData = async () => {
+    //         return {
+    //             replies: await db.Auctions.Items.Comments.Replies.findOneCommentAllReplies(auctionId, id, comment.id),
+    //             ...comment
+    //         };
+    //     }
+    //     return getData();
+    // }), [comments])
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const [comment, setComment] = useState("");
+
+    const addComment = () => {
+        db.Auctions.Items.Comments.addComment(auctionId, id, { userId: user.id, timestamp: new Date(), comment })
+        setComment(""); //clear TextField
+    }
+
     const attemptBid = () => {
         if (user) {
             setClassicModal(true)
@@ -52,7 +82,7 @@ export default function Item({ auctionId, id, name, description, picture }) {
             history.push("/login")
         }
     }
-    
+
     // const [highestBidQuery, setHighestBid] = useState([])
     // useEffect(() => {
     //     db.Auctions.Items.Bids.findHighest(auctionId, id, setHighestBid)
@@ -74,7 +104,7 @@ export default function Item({ auctionId, id, name, description, picture }) {
 
     const [amount, setAmount] = useState(0)
 
-    
+
 
     const highestBid = () => {
         return Math.max(...bids.map(bid => bid.amount), 0)
@@ -146,7 +176,10 @@ export default function Item({ auctionId, id, name, description, picture }) {
                                     {/*show bid if auction did not finish + item is available for bids*/}
                                     <Button color="primary" size="lg" onClick={() => setClassicModal(true)}>
                                         Bid
-                        </Button>
+                                    </Button>
+                                    <Button color="primary" size="lg" onClick={handleExpandClick}>
+                                        View Comments
+                                    </Button>
                                 </CardFooter>
                                 {
                                     user && user.role == 'admin' &&
@@ -162,7 +195,32 @@ export default function Item({ auctionId, id, name, description, picture }) {
                                     </>
                                 }
 
+                                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                    <CardContent>
+                                        {comments.length > 0
+                                            ? comments.map(comment =>
+                                                <Comment key={comment.id} auctionId={auctionId} itemId={id} {...comment} />)
+                                            : <Info>No questions found, be the first to leave one!</Info>}
+                                        <TextField
+                                            label="Ask a Question"
+                                            multiline
+                                            rows={1}
+                                            rowsMax={Number.MAX_SAFE_INTEGER}
+                                            value={comment}
+                                            onChange={(event) => setComment(event.target.value)}
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment>
+                                                        <IconButton color="primary" onClick={addComment}>
+                                                            <SendIcon />
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                        />
 
+                                    </CardContent>
+                                </Collapse>
                             </Card>
 
                         </GridItem>
